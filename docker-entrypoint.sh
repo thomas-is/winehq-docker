20291e9ba46579f136ac4694a3750a7b926a1d41
thomas-is <thomas@0l.fi>
2022-05-20 09:16:16 +0200

    :shell: improve docker-entrypoint
    - handle symlink to ~/local-settings if needed
    - add colors

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,22 +1,49 @@
 #!/bin/bash
 
-groupmod -g $VIDEO_GID video
-groupmod -g $AUDIO_GID audio
+RST="\033[0m"
+RED="\033[1;31m"
+GRN="\033[1;32m"
+ORG="\033[1;33m"
+BLU="\033[1;34m"
 
-usermod -a -G video  wine
-usermod -a -G audio  wine
-usermod wine -u $USER_ID
+info() {
+  ARG="$@"
+  printf "[${BLU}entrypoint$RST] ${GRN}$ARG$RST\n"
+}
+warn() {
+  ARG="$@"
+  printf "[${BLU}entrypoint$RST] ${ORG}$ARG$RST\n"
+}
+run() {
+  CMD="$@"
+  printf "[${BLU}entrypoint$RST] $CMD\n"
+#  $CMD
+}
+
+info setup audio and video groups
+run groupmod -g $VIDEO_GID video
+run groupmod -g $AUDIO_GID audio
+run usermod -a -G video  wine
+run usermod -a -G audio  wine
+info setup user id
+run usermod wine -u $USER_ID
 
 ## skip MONO install on init
 #su wine -c "WINEDLLOVERRIDES=\"mscoree=\" wineboot"
+info boot wine
+run su wine -c "wineboot"
 
-su wine -c "wineboot"
+if [ -d "/home/wine/local-settings" ] ; then
+  LINK="/home/wine/.wine/drive_c/users/wine/Local Settings"
+  info "setup symlink to ~/local-settings"
+  rm -rf "$LINK"
+  ln -s /home/wine/local-settings "$LINK"
+fi
 
 # winetricks section
 if [ "$WINETRICKS" != "" ] ; then
-  su wine -c "winetricks $WINETRICKS"
+  info "winetricks setup"
+  run su wine -c "winetricks $WINETRICKS"
 fi
 
-CMD="$@"
-su wine -c "$CMD"
-#exec "$@"
+exec "$@"
