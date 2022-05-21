b70146c37e9239956dc8143119a94a1de141fa21
thomas-is <thomas@0l.fi>
2022-05-21 10:51:25 +0200

    :shell: fix version number

--- versions.sh
+++ versions.sh
@@ -4,7 +4,7 @@
 for DEBIAN in buster bullseye testing
 do
   printf "%12s: " $DEBIAN
-  WINE=$( docker run --rm -it i386/debian:$DEBIAN-slim bash -c "apt-get update > /dev/null && apt-cache show wine | grep Version | sed 's/^Version: //g' | cut -f1 -d'-'" 2> /dev/null)
+  WINE=$( docker run --rm -it i386/debian:$DEBIAN-slim bash -c "apt-get update > /dev/null && apt-cache show wine | grep Version | sed 's/^Version: //g' | cut -f1 -d'-' | sed 's/~.*//g'" 2> /dev/null)
   printf "wine-$WINE\n"
 done
 
