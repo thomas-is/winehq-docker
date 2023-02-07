b61b21930199763624d185f5b4d5b49f793913a2
thomas-is <thomas@0l.fi>
2023-02-07 08:24:10 +0100

    🐚 use docker-run

--- winehq/test.sh
+++ winehq/test.sh
@@ -1,32 +1,10 @@
 #!/bin/bash
 
-#ROOT_DIR="/home/thomas/games/$1"
-ROOT_DIR="/home/thomas/games/test"
-WINETRICKS=""
-WINEDLLOVERRIDES=""
-. ../launcher/init-dir.sh
+docker build --build-arg DEBIAN=bookworm -t winehq . || exit 1
 
-docker build --build-arg DEBIAN=bookworm -t wine . \
-&& docker run --rm -it \
-  --name wine \
-  --device /dev/dri \
-  --device /dev/input \
-  --hostname $( hostname ) \
-  --ipc="host" \
-  -e USER_ID=$(id -u) \
-  -e VIDEO_GID=$(  cat /etc/group | grep video  | cut -f3 -d":" ) \
-  -e INPUT_GID=$(  cat /etc/group | grep input | cut -f3 -d":" ) \
-  -e WINETRICKS="${WINETRICKS:-isolate_home}" \
-  -e WINEDLLOVERRIDES="$WINEDLLOVERRIDES" \
-  -e HOME=/home/wine \
-  -e DISPLAY=unix$DISPLAY \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -v $HOME/.Xauthority:/home/wine/.Xauthority:ro \
-  -e PULSE_SERVER=unix:/pulse \
-  -v /run/user/$(id -u)/pulse/native:/pulse \
-  -v $ROOT_DIR/app:/home/wine/app \
-  -v $ROOT_DIR/install:/home/wine/install \
-  -v $ROOT_DIR/user:/home/wine/user \
-  -v $ROOT_DIR/wineprefix:/home/wine/.wine \
-  -w /home/wine/app \
-  wine
+ROOT_DIR="/home/thomas/games/test"
+WINETRICKS="isolate_home vd=1920x1080 d3dx9_41 renderer=gl videomemorysize=1024"
+WINEDLLOVERRIDES=""
+#RUN_EXE="Vampire.exe -game Unofficial_Patch -console"
+
+. $(dirname $(realpath $0) )/docker-run.sh
