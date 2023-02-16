fabea3d71eda890cdd43b833d8917ca4a3672363
thomas-is <thomas@0l.fi>
2023-02-16 15:11:37 +0100

    :shell: add codenames

--- winehq/version.sh
+++ winehq/version.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
-DEBIAN=bookworm
+VERSION=$( cat << EOF
+bookworm
+bullseye
+buster
+stretch
+jessie
+wheezy
+EOF
+)
 
-WINEHQ=$( curl -s https://dl.winehq.org/wine-builds/debian/dists/$DEBIAN/main/binary-i386/Packages \
-  | grep Version \
-  | sort \
-  | uniq \
-  | tail -n 1 \
-  | cut -f2 -d":" \
-  | cut -f1 -d"~" \
-  | tr -d " " )
-
-echo "winehq:$WINEHQ-$DEBIAN"
-
+for DEBIAN in $VERSION
+do
+  WINEHQ=$( curl -s https://dl.winehq.org/wine-builds/debian/dists/$DEBIAN/main/binary-i386/Packages \
+    | grep Version \
+    | sort \
+    | uniq \
+    | tail -n 1 \
+    | cut -f2 -d":" \
+    | cut -f1 -d"~" \
+    | tr -d " " )
+  echo "winehq:$WINEHQ-$DEBIAN"
+done
