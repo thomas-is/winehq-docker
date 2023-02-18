b1d6c63af54264fcc23f6fcb106a049038de16c5
thomas-is <thomas@0l.fi>
2023-02-18 12:25:30 +0100

    📦 change directory tree

--- winehq/test.sh
+++ winehq/test.sh
@@ -6,6 +6,7 @@ ROOT_DIR="/home/thomas/games/test"
 WINETRICKS="isolate_home vd=1920x1080 d3dx9_41 renderer=gl videomemorysize=1024"
 WINEDLLOVERRIDES=""
 #RUN_EXE="Vampire.exe -game Unofficial_Patch -console"
+SET_LAA="Vampire.exe"
 
 mkdir -p $ROOT_DIR
-. $(dirname $(realpath $0) )/bin/docker-run.sh
+. $(dirname $(realpath $0) )/games/shared/docker-run
