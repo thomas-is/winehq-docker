b0354f07af2087b76e701f38a37fac5f64f1abdc
thomas-is <thomas@0l.fi>
2023-06-20 23:40:02 +0200

    🐛 fix sorting a.b.c.d versions

--- include.sh
+++ include.sh
@@ -26,9 +26,9 @@ curl -s https://dl.winehq.org/wine-builds/debian/dists/${1:-$TESTING}/main/binar
   | grep -A2 "Package: winehq-${2:-stable}" \
   | grep "Version:" \
   | awk '{ print $2 }' \
-  | sort -t. -k 1,1n -k 2,2n \
+  | sort -t. -k 1,1nr -k 2,2nr -k 3,3nr -k 4,4nr \
   | uniq \
-  | tail -n 1 \
+  | head -n 1 \
   | cut -f2 -d":" \
   | tr -d " "
 }
